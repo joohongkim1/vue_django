0eba563c10c3fe6d32401f4aa42bb126a9c08a6d
joohongkim1 <wnghd9938@naver.com>
2019-11-19 17:25:16 +0900

    Finish create-todo

--- todo-front/src/views/Home.vue
+++ todo-front/src/views/Home.vue
@@ -1,14 +1,30 @@
 <template>
-  <div>
-    
+  <div class="home">
+    <h1>Todo</h1>
+    <!-- TodoList 렌더링 -->
+    <TodoInput @createTodo="createTodo" />
+    <TodoList :todos="todos" /> 
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import jwtDecode from 'jwt-decode' // JWT 의 payload 값을 해석해서 보여주는 library
+import TodoList from '@/components/TodoList' // 호출
+import TodoInput from '@/components/TodoInput'
 import router from '@/router' // src 로부터 router import
 
 export default {
   name: 'Home',
+  data() {
+    return { // 오브젝트를 리턴
+      todos: [],  
+    }
+  },
+  components: {
+    TodoList,
+    TodoInput,
+  }, // 등록 
 
   // 함수
   methods: {
@@ -24,12 +40,62 @@ export default {
         // 없다면 로그인 페이지로 보내겠다.
         router.push('/login')
       }
-    }
+    },
+
+    // 우리가 만든 django API 서버로 todos 를 달라는 요청을 보낸 뒤
+    // todos data 에 할당하는 함수
+    getTodo() {
+      this.$session.start()
+      const token = this.$session.get('jwt')
+      const userId = jwtDecode(token).user_id
+      const SERVER_IP = process.env.VUE_APP_SERVER_IP
+
+      const options = {
+        headers: {
+          Authorization: 'JWT ' + token
+        }
+      }
+
+      axios.get(`${SERVER_IP}/api/v1/users/${userId}/`, options)
+        .then(response => {
+          this.todos = response.data.todo_set
+        })
+        .catch(error => {
+          console.error(error)
+        })
+    },
+
+    createTodo(title) {
+      this.$session.start()
+      const token = this.$session.get('jwt')
+      const userId = jwtDecode(token).user_id
+      const SERVER_IP = process.env.VUE_APP_SERVER_IP
+      const options = {
+        headers: {
+          Authorization: 'JWT ' + token
+        }
+      }
+      // 요청을 보낼 데이터를 작성
+      const data = {
+        title, 
+        user: userId
+      }
+
+      axios.post(`${SERVER_IP}/api/v1/todos/`, data, options)
+        .then(response => {
+          this.todos.push(response.data)
+        })
+        .catch(error => {
+          console.error(error)
+        })
+
+    },
   },
 
   // Vue 가 화면에 그려지면 실행하는 함수
   mounted() {
-
+    this.checkLoggedIn()
+    this.getTodo()
   }
 
 }
